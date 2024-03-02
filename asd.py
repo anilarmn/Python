d042b2da800a6b50a839237aeafdeec83df33705
anilarmn <161921030+anilarmn@users.noreply.github.com>
2024-03-02 12:57:13 +0300

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,53 @@
+print ('Merhaba') 
+print ("Nasılsın?")
+print ('''Nasılsın?''')
+print('''Eren'in annesi "gel" dedi''');
+
+print("numara \teşya")
+print("1\ttabak")
+print("2\tçatal")
+print("3\tbıçak")
+
+a=55
+b=65
+print("a","b")
+print("a"+"b")
+print("a" * 100 )
+
+print("a+b")
+
+
+# 201 ╔ # 205 # 187 ╗ # 186 ║ # 200 ╚ # 188 ╝
+
+print("run coding") #başlık
+
+print("╔=========================╗")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("║                         ║")
+print("╚=========================╝")
+
+
+
+
+
+
+
+
+
+
